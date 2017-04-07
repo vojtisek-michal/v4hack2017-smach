b88946a226d03de23020b43aba63617cf581b619
Petr Martinec <petr.martinec@gmail.com>
2017-04-07 19:53:12 +0200

    Back-end API test -> OK

--- raspberry/smach.py
+++ raspberry/smach.py
@@ -1,18 +1,38 @@
 #!/usr/bin/env python
 
 import argparse
+import urllib2
 
-
+# ==API==
+#
+# charging/start:
+#  <- charging_session_id
+#
+# charging/update?charging_session_id=
+#  &watt_current=
+#  &watt_total=
+#  &amp_current=
+#  &amp_set=
+#
+# charging/stop?charging_session_id=
+#
+# get-current-amp?charging_session_id=
+#  <- amp_set
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('apiurl',
 	metavar='API_URL',
-    help='URL adress of API endpoint'
-	);
+    help='URL adress of API endpoint',
+	)
+
 
 args = parser.parse_args()
 
-url = args.apiurl
+API_URL = args.apiurl
 
-print url
+#print url
+
+charging_id = urllib2.urlopen(API_URL + "/charging/start").read()
+
+print "chargingId:", charging_id
